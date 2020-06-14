63411cc77024c98842c37c534ab224b54c0dffe7
xulch <4355661175@qq.com>
2020-06-14 23:04:24 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Button disabled>Hello</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
