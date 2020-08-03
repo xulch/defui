68874ae12f51e8e4c172f8faff259924c8125fda
xulch <4355661175@qq.com>
2020-08-03 09:42:31 +0800

    update

---
+++ storybook-static/precache-manifest.0a05d4c378f5426232a424a01695dea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83276bad3ec64c383add7f4eca39c1fb",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.092220c7149cb9ee7c9c.bundle.js"
+  },
+  {
+    "url": "runtime~main.092220c7149cb9ee7c9c.bundle.js"
+  },
+  {
+    "revision": "9f8307ee8b06e98abbf8",
+    "url": "static/css/main.913eec0b.chunk.css"
+  },
+  {
+    "revision": "62e7cc584d093b221f9d",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.092220c7149cb9ee7c9c.bundle.js"
+  },
+  {
+    "url": "vendors~main.092220c7149cb9ee7c9c.bundle.js.LICENSE.txt"
+  }
+]);
